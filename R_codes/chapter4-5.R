f48f587fc8f233248068c2fcaed18eeaadfadb73
marysalvana <mary.salvana@obf.ateneo.edu>
2021-04-03 11:05:00 +0300

    edited application plot

--- R_codes/chapter4-5.R
+++ R_codes/chapter4-5.R
@@ -23,54 +23,28 @@ nc_close(ncin)
 
 TT <- dim(u_array)[3]
 
-zlim_range <- range(u_array, na.rm = T)
-
 cat("PLOTTING...", '\n')
-
-for(tt in 1:3){
-	jpeg(file = paste(root, 'Figures/4_application_', tt,'.jpg', sep = ''), width = 1500, height = 900)
-
-	split.screen( rbind(c(0.05,0.96,0.1,0.93), c(0.93,0.99,0.1,0.95)))
-
-	screen(1)
-	par(mai=c(0.5,0.5,0.5,0.5))
-	quilt.plot(c(lon), c(lat), u_array[, , tt], zlim = zlim_range, nx = 1000, ny = 1000, ylab = '', xlab = '', cex.lab = 4, add.legend = F, cex.axis = 3, tick = F, line = 0.5)
-	map("state", xlim =  c(-120, -70), ylim = c(30, 50), lwd = 0.75, add = T)
-
-	mtext('Longitude', side = 1, line = 4.5, adj = 0.5,  cex = 3.5, font = 2)
-	mtext('Latitude', side = 2, line = 4.5, adj = 0.5, cex = 3.5, font = 2)
-
-	#screen(2)
-	#x1 <- c(0.025,0.12,0.12,0.025)
-	#y1 <- c(0.25,0.25,0.7,0.7)
-	#legend.gradient2(cbind(x1,y1), title = "", limits = round(seq(-zlim_range, zlim_range,length.out = 5), 1), cex = 3)
-
-	close.screen( all=TRUE)
-	dev.off()
-}
 
 saudi <- map("state", "Virginia", fill = TRUE)
 
 IDs <- sapply(strsplit(saudi$names, ":"), function(x) x[1])
 saudi <- map2SpatialPolygons(saudi, IDs=IDs, proj4string=CRS("+proj=longlat +datum=WGS84"))
 
-test1 <- data.frame(c(lon), c(lat), c(u_array[,, 1]), c(u_array[,, 2]), c(u_array[,, 3]), c(u_array[,, 4]), c(u_array[,, 5]))
-colnames(test1) <- c('lon', 'lat', 'Z1', 'Z2', 'Z3', 'Z4', 'Z5')
+test1 <- data.frame(c(lon), c(lat), c(u_array[,, 1]), c(u_array[,, 2]), c(u_array[,, 3]), c(u_array[,, 4]), c(u_array[,, 5]), c(u_array[,, 6]), c(u_array[,, 7]), c(u_array[,, 8]), c(u_array[,, 9]), c(u_array[,, 10]))
+colnames(test1) <- c('lon', 'lat', 'Z1', 'Z2', 'Z3', 'Z4', 'Z5', 'Z6', 'Z7', 'Z8', 'Z9', 'Z10')
 spdf <- SpatialPointsDataFrame(coords = test1[, c("lon", "lat")], data = test1,
 			       proj4string = CRS("+proj=longlat +datum=WGS84"))
 
 DAT_SUB <- data.frame(spdf[!is.na(over(spdf, as(saudi, "SpatialPolygons"))), ])
 
-zlim_range <- range(DAT_SUB[, 3:7], na.rm = T)
+zlim_range <- range(DAT_SUB[, 3:12], na.rm = T)
 
-hr_ind <- 1:5
-label <- c('00:00', '00:05', '00:10', '00:15', '00:20')
+hr_ind <- 1:10
+label <- c('00:00', '00:05', '00:10', '00:15', '00:20', '00:25', '00:30', '00:35', '00:40', '00:45')
 
-asratio = 1
+pdf(file = paste(root, 'Figures/4_application.pdf', sep = ''), width = 25, height = 4)
 
-jpeg(file = paste(root, 'Figures/4_application.jpg', sep = ''), width = 2000, height = 500)
-
-split.screen( rbind(c(0.08,0.95,0.1,0.95), c(0.95,0.99,0.1,0.95)))
+split.screen( rbind(c(0.05,0.95,0.1,0.95), c(0.95,0.99,0.1,0.95)))
 split.screen( figs = c( 1, 5 ), screen = 1 )
 
 hr_count <- 0
@@ -80,15 +54,16 @@ for(hr in hr_ind){
 	
 	screen(2 + hr_count)
 
-	par(pty = 's')
-	par(mai=c(0.2,0.2,0.2,0.2))
+	#par(pty = 's')
+	#par(mai=c(0.2,0.2,0.2,0.2))
+	par(pin=c(4, 2))
 	
-	quilt.plot(DAT_SUB[, 1], DAT_SUB[, 2], DAT_SUB[, 2 + hr], zlim = zlim_range, nx = 600, ny = 100, ylab = '', xlab = '', cex.lab = 4, add.legend = F, cex.axis = 2, axes = F, asp = asratio)
+	quilt.plot(DAT_SUB[, 1], DAT_SUB[, 2], DAT_SUB[, 2 + hr], zlim = zlim_range, nx = 600, ny = 100, ylab = '', xlab = '', cex.lab = 4, add.legend = F, cex.axis = 2, axes = F)
 
 	if(hr_count == 1){
 		axis(1, cex.axis = 2)
-		axis(2, cex.axis = 2)
-		mtext('Base Reflectivity (dBZ)', side = 2, line = 7, adj = 0.5, cex = 3, font = 2, col = 'blue')
+		axis(2, at = c(37, 38, 39), cex.axis = 2)
+		#mtext('Base Reflectivity (dBZ)', side = 2, line = 7, adj = 0.5, cex = 3, font = 2, col = 'blue')
 	}else{
 		axis(1, cex.axis = 2)
 	}
@@ -105,79 +80,53 @@ screen(2)
 
 x1 <- c(0.025,0.12,0.12,0.025) + 0.1
 y1 <- c(0.3,0.3,0.7,0.7)
-legend.gradient2(cbind(x1,y1), title = "", limits = round(seq(zlim_range[1], zlim_range[2], length.out = 5), 1), CEX = 2)
+legend.gradient2(cbind(x1,y1), title = "", limits = round(seq(zlim_range[1], zlim_range[2], length.out = 3), 1), CEX = 1.5)
 
 close.screen( all=TRUE)
 dev.off()
 
 
+pdf(file = paste(root, 'Figures/4_application.pdf', sep = ''), width = 27, height = 7)
 
-for(tt in 1:3){
+split.screen( rbind(c(0.05,0.95,0.12,0.92), c(0.95,0.99,0.12,0.92)))
+split.screen( figs = c( 2, 5 ), screen = 1 )
 
-	jpeg(file = paste(root, 'Figures/4_application_FULL_', tt,'.jpg', sep = ''), width = 1500, height = 900)
+hr_count <- 0
+for(hr in hr_ind){
+	
+	hr_count <- hr_count + 1
+	
+	screen(2 + hr_count)
 
-	split.screen( rbind(c(0.05,0.96,0.1,0.93), c(0.93,0.99,0.1,0.95)))
+	#par(pty = 's')
+	par(pin=c(6, 1.5))
+	par(mai=c(0.5,0.5,0.5,0.6))
+	
+	quilt.plot(DAT_SUB[, 1], DAT_SUB[, 2], DAT_SUB[, 2 + hr], zlim = zlim_range, nx = 900, ny = 150, ylab = '', xlab = '', cex.lab = 4, add.legend = F, cex.axis = 2, axes = F, xlim = c(-82, -76))
 
-	screen(1)
-	par(mai=c(0.5,0.5,0.5,0.5))
-	quilt.plot(DAT_SUB[, 1], DAT_SUB[, 2], DAT_SUB[, 3], zlim = zlim_range, nx = 200, ny = 200, ylab = '', xlab = '', cex.lab = 4, add.legend = F, cex.axis = 3, tick = F, line = 0.5)
+	#mtext('Base Reflectivity (dBZ)', side = 2, line = 7, adj = 0.5, cex = 3, font = 2, col = 'blue')
 	map("state", xlim =  c(-120, -70), ylim = c(30, 50), lwd = 0.75, add = T)
-
-	mtext('Longitude', side = 1, line = 4.5, adj = 0.5,  cex = 3.5, font = 2)
-	mtext('Latitude', side = 2, line = 4.5, adj = 0.5, cex = 3.5, font = 2)
-
-	#screen(2)
-	#x1 <- c(0.025,0.12,0.12,0.025)
-	#y1 <- c(0.25,0.25,0.7,0.7)
-	#legend.gradient2(cbind(x1,y1), title = "", limits = round(seq(-zlim_range, zlim_range,length.out = 5), 1), cex = 3)
-
-	close.screen( all=TRUE)
-	dev.off()
-
-}
-
-FULL <- T
-
-if(!FULL){
-	saudi <- map("state", "Virginia", fill = TRUE)
-
-	IDs <- sapply(strsplit(saudi$names, ":"), function(x) x[1])
-	saudi <- map2SpatialPolygons(saudi, IDs=IDs, proj4string=CRS("+proj=longlat +datum=WGS84"))
-
-
-	for(tt in 1:3){
-
-		test1 <- data.frame(c(lon), c(lat), c(u_array[,, tt]))
-		colnames(test1) <- c('lon', 'lat', 'Z1')
-		spdf <- SpatialPointsDataFrame(coords = test1[, c("lon", "lat")], data = test1,
-					       proj4string = CRS("+proj=longlat +datum=WGS84"))
-
-		DAT_SUB <- data.frame(spdf[!is.na(over(spdf, as(saudi, "SpatialPolygons"))), ])
-
-		zlim_range <- range(DAT_SUB[, 3], na.rm = T)
-
-		jpeg(file = paste(root, 'Figures/4_application_FULL_', tt,'.jpg', sep = ''), width = 1500, height = 900)
-
-		split.screen( rbind(c(0.05,0.96,0.1,0.93), c(0.93,0.99,0.1,0.95)))
-
-		screen(1)
-		par(mai=c(0.5,0.5,0.5,0.5))
-		quilt.plot(DAT_SUB[, 1], DAT_SUB[, 2], DAT_SUB[, 3], zlim = zlim_range, nx = 200, ny = 200, ylab = '', xlab = '', cex.lab = 4, add.legend = F, cex.axis = 3, tick = F, line = 0.5)
-		map("state", xlim =  c(-120, -70), ylim = c(30, 50), lwd = 0.75, add = T)
-
-		mtext('Longitude', side = 1, line = 4.5, adj = 0.5,  cex = 3.5, font = 2)
-		mtext('Latitude', side = 2, line = 4.5, adj = 0.5, cex = 3.5, font = 2)
-
-		#screen(2)
-		#x1 <- c(0.025,0.12,0.12,0.025)
-		#y1 <- c(0.25,0.25,0.7,0.7)
-		#legend.gradient2(cbind(x1,y1), title = "", limits = round(seq(-zlim_range, zlim_range,length.out = 5), 1), cex = 3)
-
-		close.screen( all=TRUE)
-		dev.off()
-
+	
+	if(hr_count %in% c(1, 6)){
+		mtext('Latitude', side = 2, line = 4, adj = 0.5, cex = 2.5, font = 2)
+		axis(2, at = c(37, 38, 39), cex.axis = 2)
+	}
+	mtext(label[hr], side = 3, line = 1, adj = 0.5, cex = 3, font = 2)
+	if(hr > 5){
+		mtext('Longitude', side = 1, line = 4, adj = 0.5,  cex = 2.5, font = 2)
+		axis(1, cex.axis = 2)
 	}
 }
+
+screen(2)
+
+x1 <- c(0.025,0.12,0.12,0.025) + 0.1
+y1 <- c(0.3,0.3,0.7,0.7)
+legend.gradient2(cbind(x1,y1), title = "", limits = round(seq(zlim_range[1], zlim_range[2], length.out = 3), 1), CEX = 2)
+
+close.screen( all=TRUE)
+dev.off()
+
 
 #Virginia, 2021-02-27T00:00:00Z 
 #2021-03-15T00:00:00Z
