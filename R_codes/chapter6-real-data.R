eaf3aaa05d66b10503aee54d0a9e4a3388c39480
marysalvana <mary.salvana@obf.ateneo.edu>
2021-04-05 23:55:43 +0300

    added codes for spacetime exageostat

---
+++ R_codes/chapter6-real-data.R
@@ -0,0 +1,228 @@
+directory <- '/home/salvanmo/Desktop/'
+
+root <- paste(directory, 'studious-potato/', sep = '')
+
+source(file = paste(root, "R_codes/Functions/load_packages.R", sep = ''))
+source(file = paste(root, "R_codes/Functions/auxiliary_functions.R", sep = ''))
+
+# LOAD HOURLY DATA FROM 2016 TO 2019
+
+start_yr <- 2016
+
+DAT <- NULL
+
+for(yr in start_yr:(start_yr + 3)){
+
+	# LOAD DATA FOR EACH YEAR, EACH YEAR HAS A 550 x 8760 MATRIX, WHERE COLUMNS ARE MEASUREMENTS IN TIME AND ROWS ARE MEASUREMENTS IN SPACE
+	cat("LOADING HOURLY DATA IN ", yr, ". . .", '\n')
+	DAT_temp <- read.table(paste('/home/salvanmo/Desktop/ipdps/Data/pm_', yr, sep = ''), header = FALSE, sep = " ") %>% as.matrix()
+	DAT <- cbind(DAT, DAT_temp)
+}
+
+# LOAD LOCATION COORDINATES
+
+locs <- read.table(paste('/home/salvanmo/Desktop/ipdps/Data/locations_550', sep = ''), header = FALSE, sep = ",")
+
+N <- nrow(locs)
+
+# TAKE THE AVERAGE OF THE MEASUREMENTS FOR TWO DAYS BECAUSE THEY YIELD APPROXIMATELY SECOND-ORDER STATIONARY MEASUREMENTS
+
+aggregate <- 48
+TT <- floor(ncol(DAT) / aggregate)		#total number of temporal locations to be analyzed
+
+DAT2 <- matrix(, ncol = TT, nrow = N)
+
+for(aa in 1:TT){
+	DAT2[, aa] <- apply(DAT[, (aa - 1) * aggregate + 1:aggregate], 1, mean)
+}
+
+# REMOVE THE SPATIO-TEMPORALLY VARYING TREND USING EMPIRICAL ORTHOGONAL FUNCTIONS
+
+Yhat1 <- res_mat1 <- obs_mat_standardized1 <- t(DAT2)
+
+obs_mat_SVD1 <- svd(obs_mat_standardized1)
+
+variance.explained1 = prop.table(obs_mat_SVD1$d^2)
+
+percent_sum_squared_variation1 <- cumsum(variance.explained1)[cumsum(variance.explained1) >= 0.9]
+min_percent_sum_squared_variation1 <- min(percent_sum_squared_variation1)
+num_singular_vec1 <- which(cumsum(variance.explained1) == min_percent_sum_squared_variation1) 
+
+X1 <- cbind(rep(1, nrow(obs_mat_SVD1$u)), obs_mat_SVD1$u)
+
+for(nn in 1:ncol(obs_mat_standardized1)){
+
+	beta_hat <- solve(t(X1[, 1:num_singular_vec1]) %*% X1[, 1:num_singular_vec1]) %*% t(X1[, 1:num_singular_vec1]) %*% obs_mat_standardized1[, nn]
+
+	Yhat1[, nn] <- Yhat_temp <- X1[, 1:num_singular_vec1] %*% beta_hat
+
+	err <- Yhat_temp - obs_mat_standardized1[, nn]
+
+	res_mat1[, nn] <- err/sd(err)
+
+}
+
+# CREATE A MATRIX OF MEASUREMENTS AND THEIR CORRESPONDING SPACE-TIME LOCATIONS: (RESIDUALS, LONGITUDE, LATITUDE, TIME) OF DIMENSION 401500 x 4 
+
+Z <- NULL
+
+for(tt in 1:TT){
+	Z <- rbind(Z, cbind(res_mat1[tt, ], cbind(locs, rep(tt, nrow(locs)))))	
+}
+
+# CHOOSE ONLY A SMALL SAMPLE WITH 100,000 SPACE-TIME MEASUREMENTS
+
+small_scale_n <- 100000
+
+set.seed(1234)
+subset_index <- sample(1:nrow(Z), small_scale_n)
+
+locs_sub <- Z[subset_index, 2:3]
+time_sub <- Z[subset_index, 4]
+Z_sub <- Z[subset_index, 1]
+
+# DIVIDE THE 100,000 DATA INTO 10 SETS OF 10, FOR 10-FOLD CROSS VALIDATION
+
+cv_n_test = 10000	# set the number or testing locations for the k-fold cross validation
+
+for(set in 1:10){
+
+	set_ind <- (set - 1) * cv_n_test + 1:cv_n_test
+	
+	# SAVING TESTING DATA
+
+	cat("SAVING TESTING DATA FOR SET ", set, "\n")
+
+	write.table(locs_sub[set_ind, 1:2], file = paste(root, "Data/locations_space_testing_set_", set, "_", cv_n_test, sep = ''), sep = ",", row.names = FALSE, col.names = FALSE)
+	write.table(time_sub[set_ind], file = paste(root, "Data/locations_time_testing_set_", set, "_", cv_n_test, sep = ''), sep = ",", row.names = FALSE, col.names = FALSE)
+	write.table(Z_sub[set_ind] - mean(Z_sub[-set_ind]), file = paste(root, "Data/data_st_testing_set_", set, "_", cv_n_test, sep = ''), sep = ",", row.names = FALSE, col.names = FALSE)
+
+	# SAVING TRAINING DATA
+
+	cat("SAVING TRAINING DATA FOR SET ", set, "\n")
+
+	write.table(locs_sub[-set_ind, 1:2], file = paste(root, "Data/locations_space_training_set_", set, "_", small_scale_n - cv_n_test, sep = ''), sep = ",", row.names = FALSE, col.names = FALSE)
+	write.table(time_sub[-set_ind], file = paste(root, "Data/locations_time_training_set_", set, "_", small_scale_n - cv_n_test, sep = ''), sep = ",",row.names = FALSE, col.names = FALSE)
+	write.table(Z_sub[-set_ind] - mean(Z_sub[-set_ind]), file = paste(root, "Data/data_st_training_set_", set, "_", small_scale_n - cv_n_test, sep = ''), sep = ",", row.names = FALSE, col.names = FALSE)
+
+}
+
+#################################################### 		FIT THE MODEL IN EXAGEOSTAT 			####################################################
+#################################################### 		RETRIEVE THE PREDICTIONS IN EXAGEOSTAT 		####################################################
+
+# LOAD PREDICTIONS TXT FILE IMPORTED FROM EXAGEOSTAT
+
+preds <- read.table(paste('/home/salvanmo/Desktop/ipdps/Predictions/predicted_values_September_08_2020_15:27:29', sep = ''), header = FALSE, sep = " ")
+
+# LOAD THE CORRESPONDING TRAINING AND TESTING DATASETS
+
+obs_TEST <- read.table(paste('/home/salvanmo/Desktop/ipdps/Data/data_st_testing_1', sep = ''), header = FALSE, sep = " ") %>% as.matrix()
+locs_s_TEST <- read.table(paste('/home/salvanmo/Desktop/ipdps/Data/locations_space_testing_1', sep = ''), header = FALSE, sep = ",") %>% as.matrix()
+locs_t_TEST <- read.table(paste('/home/salvanmo/Desktop/ipdps/Data/locations_time_testing_1', sep = ''), header = FALSE, sep = " ") %>% as.matrix()
+
+obs_TRAIN <- read.table(paste('/home/salvanmo/Desktop/ipdps/Data/data_st_training_1', sep = ''), header = FALSE, sep = " ") %>% as.matrix()
+locs_s_TRAIN <- read.table(paste('/home/salvanmo/Desktop/ipdps/Data/locations_space_training_1', sep = ''), header = FALSE, sep = ",") %>% as.matrix()
+locs_t_TRAIN <- read.table(paste('/home/salvanmo/Desktop/ipdps/Data/locations_time_training_1', sep = ''), header = FALSE, sep = " ") %>% as.matrix()
+
+# CHOOSE 1 TEMPORAL LOCATION TO PLOT THE RESULTS. CHOOSE THE TEMPORAL LOCATION WITH THE MOST NUMBER OF SPATIAL MEASUREMENTS IN THE TRAINING SET
+
+#time_index_for_plotting = which.max(count(locs_t_TRAIN)[, 2])
+
+time_index_for_plotting = 413
+
+ind_sub_test <- which(locs_t_TEST == time_index_for_plotting)		#indices of measurements in testing set at time point time_index_for_plotting
+ind_sub_train <- which(locs_t_TRAIN == time_index_for_plotting)		#indices of measurements in training set at time point time_index_for_plotting
+
+locs_s_sub_train <- locs_s_TRAIN[ind_sub_train, ]			#extract all spatial locations in training set at time point time_index_for_plotting
+locs_t_sub_train <- locs_t_TRAIN[ind_sub_train, ]			#extract all temporal locations in training set at time point time_index_for_plotting
+obs_sub_train <- obs_TRAIN[ind_sub_train]				#extract all spatial measurements in training set at time point time_index_for_plotting
+
+locs_FULL <- rbind(locs_s_sub_train, locs_s_TEST[ind_sub_test, ])	#concatenate the spatial locations from the chosen training and testing sets 
+obs_FULL <- c(obs_sub_train, obs_TEST[ind_sub_test])			#concatenate the spatial measurements from the chosen training and testing sets
+
+
+locs_s_sub_test <- locs_s_TEST[ind_sub_test, ]			#extract all spatial locations in testing set at time point time_index_for_plotting
+locs_t_sub_test <- locs_t_TEST[ind_sub_test, ]			#extract all temporal locations in testing set at time point time_index_for_plotting
+obs_sub_test <- obs_TEST[ind_sub_test]				#extract all spatial measurements in testing set at time point time_index_for_plotting
+
+obs_sub_pred <- preds[ind_sub_test, ]				#extract all spatial measurements in predictions file at time point time_index_for_plotting
+
+zlim_range <- range(c(obs_sub_pred, obs_sub_pred, obs_sub_train))
+
+jpeg(file = paste(root, 'Figures/6-application_data_predictions.jpg', sep = ''), width = 1800, height = 700)
+
+split.screen( rbind(c(0.08,0.95,0.1,0.95), c(0.95,0.99,0.1,0.95)))
+split.screen( figs = c( 1, 3 ), screen = 1 )
+
+screen(3)
+par(pty = 's')
+
+quilt.plot(locs_s_sub_train[, 1], locs_s_sub_train[, 2], obs_sub_train, ylab = '', xlab = '', cex.lab = 4, add.legend = F, cex.axis = 2, nx = 25, ny = 25, zlim = zlim_range)
+map("worldHires", xlim = c(26.719, 85.078), ylim = c(5.625, 42.188), lwd = 0.75, add = T)
+
+mtext('Latitude', side = 2, line = 4, adj = 0.5, cex = 2.5, font = 2)
+
+mtext('Longitude', side = 1, line = 4, adj = 0.5,  cex = 2.5, font = 2)
+
+mtext('Training', side = 3, line = 1, adj = 0.5, cex = 2.5, font = 2)
+
+screen(4)
+par(pty = 's')
+
+quilt.plot(c(locs_s_sub_train[, 1], locs_s_sub_test[, 1]), c(locs_s_sub_train[, 2], locs_s_sub_test[, 2]), c(obs_sub_train, obs_sub_pred), ylab = '', xlab = '', cex.lab = 4, add.legend = F, cex.axis = 2, nx = 25, ny = 25, zlim = zlim_range)
+map("worldHires", xlim = c(26.719, 85.078), ylim = c(5.625, 42.188), lwd = 0.75, add = T)
+
+mtext('Longitude', side = 1, line = 4, adj = 0.5,  cex = 2.5, font = 2)
+
+mtext('Training & Predictions', side = 3, line = 1, adj = 0.5, cex = 2.5, font = 2)
+
+screen(5)
+par(pty = 's')
+
+err <- (obs_sub_pred - obs_sub_test)^2
+
+quilt.plot(locs_s_sub_test[, 1], locs_s_sub_test[, 2], err, ylab = '', xlab = '', cex.lab = 4, add.legend = F, cex.axis = 2, nx = 25, ny = 25)
+map("worldHires", xlim = c(26.719, 85.078), ylim = c(5.625, 42.188), lwd = 0.75, add = T)
+
+mtext('Longitude', side = 1, line = 4, adj = 0.5,  cex = 2.5, font = 2)
+
+mtext('(Testing - Prediction)^2', side = 3, line = 1, adj = 0.5, cex = 2.5, font = 2)
+
+screen(2)
+
+x1 <- c(0.025,0.12,0.12,0.025) + 0.1
+y1 <- c(0.3,0.3,0.7,0.7)
+legend.gradient2(cbind(x1,y1), title = "", limits = round(seq(min(err), max(err), length.out = 5), 1), CEX = 2)
+
+close.screen( all=TRUE)
+dev.off()
+
+
+###########################  NEW TESTING FOR FULL MAP ###########################  413, 1, 700
+
+T_sub <- 413
+
+ind_sub_train <- which(locs_t_TRAIN == T_sub)
+
+locs_s_sub_train <- locs_s_TRAIN[ind_sub_train, ]
+
+new_test_ind <- NULL
+
+for(qq in 1:nrow(locs_s_sub_train)){
+	new_test_ind <- c(new_test_ind, which(locs[, 1] == locs_s_sub_train[qq, 1] & locs[, 2] == locs_s_sub_train[qq, 2]))
+}
+
+Z_vec <- Z[T_sub, -new_test_ind]
+
+locs_s_sub <- locs[-new_test_ind, ]
+Z_sub <- Z_vec
+locs_t_sub <- rep(T_sub, nrow(locs_s_sub))
+
+
+Z_train <- Z[T_sub, new_test_ind]
+locs_s_obs <- locs[new_test_ind, ]
+
+#write.table(locs_s_sub, file = "/home/salvanmo/Desktop/ipdps/Data/locations_space_testing_4", sep = ",",row.names = FALSE, col.names = FALSE)
+#write.table(locs_t_sub, file = "/home/salvanmo/Desktop/ipdps/Data/locations_time_testing_4", sep = ",",row.names = FALSE, col.names = FALSE)
+#write.table(Z_sub, file = "/home/salvanmo/Desktop/ipdps/Data/data_st_testing_4", sep = ",", row.names = FALSE, col.names = FALSE)
+
