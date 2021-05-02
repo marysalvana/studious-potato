fd444a0a659b286e723de7aa0b54edde2f07c532
marysalvana <mary.salvana@obf.ateneo.edu>
2021-05-02 13:13:11 +0300

    edited empirical analysis codes

--- R_codes/extra/empirical-analysis.R
+++ R_codes/extra/empirical-analysis.R
@@ -1,13 +1,20 @@
 
-directory <- '/home/salvanmo/Desktop/'
+#The data should reflect the true unknown covariance model which generated it.
+#Meaning: if the smoothness parameter is high, the data should show a smooth random field.
+#Meaning: if the spatial correlation is weak, it should show as well.
+#So, when we compute the sample covariance from the data, it should be a rough estimate of the true unknown covariance.
 
-root <- paste(directory, 'studious-potato/', sep = '')
+#Step 1: compute the sample covariance
 
-source(file = paste(root, "R_codes/Functions/load_packages.R", sep = ''))
-source(file = paste(root, "R_codes/Functions/cov_func.R", sep = ''))
-source(file = paste(root, "R_codes/Functions/auxiliary_functions.R", sep = ''))
+#Step 2: Plot dist vs. covariance for easier analysis
 
-N <- 50
+#Step 3: Try to fit a curve on the computed sample covariance based on a theoretical covariance model.
+#The parameters you used should be a good guess to the MLE parameters.
+
+library(MASS)
+library(dplyr)
+
+N <- 40
 n <- N^2
 grid_x <- seq(from = 0, to = 1, length.out = N)
 grid_y <- seq(from = 0, to = 1, length.out = N)
@@ -15,22 +22,33 @@ locations <- expand.grid(grid_x, grid_y) %>% as.matrix()
 
 cat('Computing covariances...', '\n')
 
+MATERN_UNI_STATIONARY <- function(PARAMETER, LOCATION){
+  
+  nu <- PARAMETER[3]
+  beta <- PARAMETER[2]
+  sigma <- PARAMETER[1]
+  
+  dist0 <- dist(x = LOCATION, diag = TRUE, upper = TRUE) %>% as.matrix()
+  
+  S <- ifelse(dist0 != 0, (dist0 / beta)^nu * besselK(dist0 / beta, nu) / (2^(nu - 1) * gamma(nu)) * sigma, sigma)
+  
+  return(S)
+}
+
 cov1 <- MATERN_UNI_STATIONARY(PARAMETER = c(1, 0.23, 1), LOCATION = locations)
 
 cat('Generating realizations...', '\n')
 
-set.seed(1)
-r1 <- rmvn(1, rep(0, ncol(cov1)), cov1, ncores = 25)
+set.seed(1234)
+DATA <- mvrnorm(1, rep(0, ncol(cov1)), cov1)
 
 cat('Remove the mean from the observations...', '\n')
 
-Z <- r1 - mean(r1)
+Z <- matrix(DATA - mean(DATA), nrow = 1)
 
 cat('Compute the empirical covariance between all possible location pairs...', '\n')
 
 emp_covariance_temp <- t(Z) %*% Z
-
-xlag <- ylag <- emp_vals <- NULL  
 
 cat('Arrange the empirical covariance values in one dataframe/matrix...', '\n')
 
@@ -45,7 +63,6 @@ empirical <- data.frame(xlag, ylag, sqrt(xlag^2 + ylag^2), emp_vals)
 colnames(empirical) <- c('xlag', 'ylag', 'dist', 'covariance_value')
 
 cat('Take the average of the empirical covariance values for each spatial lag...', '\n')
-
 
 binned_temp <- empirical %>% group_by(dist) %>% summarize(avg1 = mean(covariance_value))
 binned <- cbind(binned_temp$dist, binned_temp$avg1)
@@ -71,11 +88,11 @@ cat('Plot the empirical covariance...', '\n')
 cat('Guess estimate of the theoretical covariance...', '\n')
 
 sigma <- 1
-beta <- 0.23
+beta <- 0.15
 nu <- 1
 dist0 <- seq(0, max(sqrt(xlag^2 + ylag^2)), length = 500)
-theoretical <- ifelse(dist0 != 0, (dist0 / beta)^nu * besselK(dist0 / beta, nu) / (2^(nu - 1) * gamma(nu)), sigma)
+theoretical <- ifelse(dist0 != 0, (dist0 / beta)^nu * besselK(dist0 / beta, nu) / (2^(nu - 1) * gamma(nu)) * sigma, sigma)
 
-plot(dist.bin, EMP_COV, pch = 20)
+plot(dist.bin, EMP_COV, pch = 20, xlab = 'Distance', ylab = 'Empirical Covariance')
 lines(dist0, theoretical, col = 'red', lwd = 2)
 
