be1896470df8b48367db21ace0a45f9221cc2385
marysalvana <mary.salvana@obf.ateneo.edu>
2021-06-05 15:29:50 +0300

    added heatmaps

--- R_codes/chapter2-5-real-data.R
+++ R_codes/chapter2-5-real-data.R
@@ -18,41 +18,16 @@ AREA <- 'SAUDI'
 
 DATA <- data_format(aggregate = 1, area = AREA)
 
+locs <- as.matrix(DATA[[2]])
+locs <- cbind((locs[, 1] - mean(locs[, 1])) / sd(locs[, 1]), (locs[, 2] - mean(locs[, 2])) / sd(locs[, 2]))
+
+
 ####################################################################################################
 
 plot_realdata_for_checking_stationarity(data_list = DATA, file_name = '2-application-saudi-data-scratch.jpg', start_hr = 8760 + 1, saudi = T)
 plot_realdata_for_manuscript(data_list = DATA, file_name = '2-application-saudi-data-scratch.jpg', start_hr = 8760 + 1, saudi = T)
 
 ####################################################################################################
-
-locs <- as.matrix(DATA[[2]])
-locs <- cbind((locs[, 1] - mean(locs[, 1])) / sd(locs[, 1]), (locs[, 2] - mean(locs[, 2])) / sd(locs[, 2]))
-Z_rand_sample <- matrix(c(DATA[[1]][8760 + 1, ], DATA[[1]][8760 + 2, ], DATA[[1]][8760 + 3, ]), nrow = 1)
-
-NEGLOGLIK <- function(p){
-
-	theta <- exp(p[1:3])
-	WIND <- p[4:5]
-	PARAMETER_NONSTAT <- p[-c(1:5)]
-
-	Sigma <- MATERN_UNI_SPATIALLY_VARYING_PARAMETERS(PARAMETER = c(theta, WIND, 0.001, 0, 0, 0.001), LOCATION = locs, TIME = 3, PARAMETER_NONSTAT = PARAMETER_NONSTAT)
-	cholmat <- t(cholesky(Sigma[['covariance']], parallel = TRUE))
-	z <- forwardsolve(cholmat, t(Z_rand_sample))
-	logsig  <- 2 * sum(log(diag(cholmat))) * nrow(Z_rand_sample)
-	out  <- 1/2 * logsig + 1/2 * sum(z^2)
-
-	return(out)
-}
-
-#init <- rep(0.001, 20)
-#fit1 <- optim(par = init, fn = NEGLOGLIK, control = list(trace = 5, maxit = 1000)) #
-
-p <- fit1$par
-PARAMETER_NONSTAT <- p[-c(1:5)]
-PARAMETER_NONSTAT
-
-fit1 <- optim(par = fit1$par, fn = NEGLOGLIK, control = list(trace = 5, maxit = 1000)) #
-
 
 
 
@@ -89,6 +64,12 @@ sigma <- htarg^2 * log(htarg)
 sigma[htarg == 0] <- 0
 
 Z_rand_sample <- matrix(DATA[[1]][8760 * 3 + 1, ], nrow = 1)
+
+
+
+####################################################################################################
+
+
 
 NEGLOGLIK_DEFORM <- function(p){
 
@@ -141,6 +122,7 @@ plot(cbind(c(parWarpsSum[, 1] %*% sigma), c(parWarpsSum[, 2] %*% sigma)), pch = 
 dev.off()
 
 
+####################################################################################################
 
 
 NEGLOGLIK_SPATIALLY_VARYING <- function(p){
@@ -165,9 +147,6 @@ NEGLOGLIK_SPATIALLY_VARYING <- function(p){
 
 	return(out)
 }
-
-#Z_rand_sample <- matrix(DATA[[1]][8760 * 2 + 1, ], nrow = 1)
-Z_rand_sample <- matrix(DATA[[1]][8760 * 3 + 1, ], nrow = 1)
 
 jWarp = 1:10
 init <- c(rep(0, 3), rep(0, 3 * length(jWarp)))
@@ -194,62 +173,7 @@ for(i in 1:3){
 }
 dev.off()
 
-#########################################################################################################
 
 
-cores=detectCores()
-number_of_cores_to_use = cores[1]-1 # not to overload the computer
-cl <- makeCluster(number_of_cores_to_use) 
-registerDoParallel(cl)
-
-number_of_chunks = number_of_cores_to_use
-
-clusterExport(cl, "root")
-clusterEvalQ(cl, source(paste(root, "R_codes/Functions/load_packages.R", sep = '')))
-clusterEvalQ(cl, source(paste(root, "R_codes/Functions/cov_func.R", sep = '')))
-clusterEvalQ(cl, sourceCpp(paste(root, "R_codes/Functions/spatially_varying_parameters2.cpp", sep = '')))
-
-clusterExport(cl, c("PARAMETER_NONSTAT", "TT"), envir = environment())
-
-set.seed(1234)
-wind_vals <- mvrnorm(100, rep(0, 2), 0.001 * diag(2))
-
-output <- foreach(i=1:nrow(wind_vals), .combine=cbind, .packages = "Rcpp", .noexport = "SPATIALLY_VARYING_PARAMETERS_FOR_FITTING_PARALLEL") %dopar% {
-	
-	COVARIANCE <- MATERN_UNI_SPATIALLY_VARYING_PARAMETERS(PARAMETER = c(1, 0.23, 1, wind_vals[i, ], 0.001, 0, 0, 0.001), LOCATION = sim_grid_locations, TIME = TT, PARAMETER_NONSTAT = PARAMETER_NONSTAT, FITTING = T, PARALLEL = T)
-
-	return(c(COVARIANCE))
-}
-
-stopCluster(cl)
-
-
-cov1 <- matrix(rowSums(output), n * TT, n * TT) / nrow(wind_vals) 
-
-set.seed(1)
-r1 <- rmvn(100, rep(0, n * TT), cov1, ncores = 25)
-
-
-#####################################################################
-
-pdf(file = paste(root, 'Figures/2-scratch-cov1-realizations.pdf', sep = ''), width = 30, height = 22)
-
-par(mfrow = c(4, 5))
-for(tt in 1:TT){
-	quilt.plot(sim_grid_locations[, 1], sim_grid_locations[, 2], r1[70, (tt - 1)* n + 1:n], nx = N, ny = N)
-}
-
-dev.off()
-
-pdf(file = paste(root, 'Figures/2-scratch-cov1-heatmap.pdf', sep = ''), width = 30, height = 22)
-
-par(mfrow = c(4, 5))
-for(tt in 1:TT){
-	image.plot(matrix(cov1[reference_locations[1], (tt - 1)* n + 1:n], N, N), zlim = c(0, 1))
-}
-for(tt in 1:TT){
-	image.plot(matrix(cov1[reference_locations[2], (tt - 1)* n + 1:n], N, N), zlim = c(0, 1))
-}
-
-dev.off()
+####################################################################################################
 
