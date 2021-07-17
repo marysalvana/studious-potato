dc749e2d15e527802074b0a2bb077574445f7f2e
marysalvana <mary.salvana@obf.ateneo.edu>
2021-07-17 10:26:30 +0300

    edited boxplots

--- R_codes/chapter5-3-estimation.R
+++ R_codes/chapter5-3-estimation.R
@@ -358,7 +358,7 @@ if(PLOT_RESULTS){
 	y0s <- matrix(c(WIND_MU, WIND_VAR[1, 1], WIND_VAR[2, 2], WIND_VAR[1, 2]), nrow = 1, ncol = ncol(params), byrow = T)
 
 
-	pdf(file = paste(root, 'Figures/5-boxplots-estimated-advection-parameters.pdf', sep = ''), width = 23, height = 10)
+	pdf(file = paste(root, 'Figures/5-boxplots-estimated-advection-parameters.pdf', sep = ''), width = 23, height = 12)
 
 	split.screen( rbind(c(0.08,0.98,0.1,0.95), c(0.98,0.99,0.1,0.95)))
 	split.screen( figs = c( 2, 3 ), screen = 1 )
@@ -375,12 +375,12 @@ if(PLOT_RESULTS){
 			screen((model - 1) * 3 + 2 + m)
 			par(mai=c(0.2,0.2,0.2,0.2))
 			
-			boxplot(params[, 1:5], xaxt = 'n', yaxt = 'n', ylim = c(-0.1, 1))
+			boxplot(params[, 1:5], xaxt = 'n', yaxt = 'n', ylim = c(-0.1, 0.5))
 			segments(x0 = x0s, x1 = x1s, y0 = y0s[1:5], col = "red", lwd = 2)
 
 			if(m == 1){
 				#mtext(expression(hat(f)), side = 2, line = 4, adj = 0.5, cex = 2.5, font = 2)
-				text(-0.6, 0.5, mod_label[model], col = 'blue', xpd = NA, cex = 4, font = 2)
+				text(-0.6, 0.2, mod_label[model], col = 'blue', xpd = NA, cex = 4, font = 2)
 				#mtext(mod_label[model], side = 2, line = 8, adj = 0.5, cex = 4, font = 2, col = 'blue')
 				axis(2, cex.axis = 2)
 			}
